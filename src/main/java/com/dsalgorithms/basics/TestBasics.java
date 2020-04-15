b1662e986b43c1b8cc70e6a5cb4334809203e550
harishyadav-t <harish.thatha@omniwyse.com>
2020-04-15 14:17:20 +0530

    Intial commit to basics changes

--- src/main/java/com/dsalgorithms/basics/TestBasics.java
+++ src/main/java/com/dsalgorithms/basics/TestBasics.java
@@ -6,15 +6,13 @@ import java.util.Queue;
 class TestBasics {
 	public static void main(String[] args) {
 		Basics basics = new Basics();
-
-		//System.out.println(basics.getNum() + " " + b2.getNum());
 		Queue<Integer> queue = new LinkedList<>();
 		queue.add(2);
 		queue.add(35);
 		queue.add(74);
 		queue.remove();
 		System.out.println(queue.peek());
-		System.out.println(basics.checkGivenName("harisha"));
+		System.out.println(basics.checkGivenName("harish"));
 
 	}
 }
